2e6c01a5b23cefd7d37d514d3861dd6a0d59240e
dweiller <4678790+dweiller@users.noreply.github.com>
2024-07-20 02:40:29 +1000

    add default-dir build option

--- zigup.zig
+++ zigup.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 const builtin = @import("builtin");
 const mem = std.mem;
 
+const build_options = @import("build-options");
+
 const ArrayList = std.ArrayList;
 const Allocator = mem.Allocator;
 
@@ -131,14 +133,14 @@ fn allocInstallDirString(allocator: Allocator) ![]const u8 {
     if (builtin.os.tag == .windows) {
         const self_exe_dir = try std.fs.selfExeDirPathAlloc(allocator);
         defer allocator.free(self_exe_dir);
-        return std.fs.path.join(allocator, &.{ self_exe_dir, "zig" });
+        return std.fs.path.join(allocator, &.{ self_exe_dir, build_options.default_dir });
     }
     const home = try getHomeDir();
     if (!std.fs.path.isAbsolute(home)) {
         std.log.err("$HOME environment variable '{s}' is not an absolute path", .{home});
         return error.BadHomeEnvironmentVariable;
     }
-    return std.fs.path.join(allocator, &[_][]const u8{ home, "zig" });
+    return std.fs.path.join(allocator, &[_][]const u8{ home, build_options.default_dir });
 }
 const GetInstallDirOptions = struct {
     create: bool,
